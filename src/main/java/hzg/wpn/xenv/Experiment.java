e5919e558ce6cbc16ac58181293970daa4941811
Ingvord <mail@ingvord.ru>
2016-02-11 18:08:41 +0100

    progress

---
+++ src/main/java/hzg/wpn/xenv/Experiment.java
@@ -0,0 +1,12 @@
+package hzg.wpn.xenv;
+
+import java.lang.Object;import java.lang.String;import java.util.Map;
+
+/**
+ * @author Igor Khokhriakov <igor.khokhriakov@hzg.de>
+ * @since 2/11/16
+ */
+public class Experiment {
+    public String id;
+    public Map<String, Object> data;
+}
